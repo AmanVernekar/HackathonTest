4cd41e4cc0a43cf9e878966cb4601a8f4efe3214
suraj-ranganath <suraj.ranganath@gmail.com>
2017-07-31 12:00:26 +0530

    lol

--- app/src/main/java/com/gumnur/firebaseui/MainActivity.java
+++ app/src/main/java/com/gumnur/firebaseui/MainActivity.java
@@ -1,13 +1,43 @@
 package com.gumnur.firebaseui;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
+    private static final String TAG = "Log";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+
+        Button btnCamera =(Button)findViewById(R.id.btnCam);
+        imageView =(ImageView)findViewById(R.id.imgView);
+
+        //Camera Button
+        btnCamera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 }
